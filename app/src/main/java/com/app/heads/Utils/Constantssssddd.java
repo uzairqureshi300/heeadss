73e6f31a42c427ba05f9024784e0ba56ed4ff734
Ali <uzair.qrsh920@gmail.com>
2018-08-18 15:56:54 +0500

    adition of constantsssss

---
+++ app/src/main/java/com/app/heads/Utils/Constantssssddd.java
@@ -0,0 +1,10 @@
+package com.app.heads.Utils;
+
+/**
+ * Created by Ali Zunair on 10/08/2018.
+ */
+
+public class Constantssssddd {
+
+    public static String base_api="http://cms.heeadsss.uk/api/";
+}
